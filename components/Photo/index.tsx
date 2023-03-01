56dea51f1ff9ef7a7917e27b1d4e715c3cd5bb67
Alison Silva <alison.silva@wundermanthompson.com>
2023-03-01 23:06:52 +0000

    Transitions on toggle dark mode

--- components/Photo/index.tsx
+++ components/Photo/index.tsx
@@ -4,7 +4,7 @@ import styles from './index.module.scss'
 export default function Photo() {
     return (
         <div className={`flex justify-center ${styles.photoMask}`}>
-            <Image src="/images/photo_alison_silva.png" className={`object-cover grayscale transition-all duration-1000 hover:grayscale-0 hover:scale-95 hover:rotate-1`} width={400} height={370} alt="Alison Silva - Full Stack Web Engineer" />
+            <Image src="/images/photo_alison_silva.png" className={`object-cover grayscale-0 dark:grayscale transition-all duration-1000 hover:grayscale hover:dark:grayscale-0 hover:scale-95 hover:rotate-1 `} width={400} height={370} alt="Alison Silva - Full Stack Web Engineer" />
         </div>
     )
 }
